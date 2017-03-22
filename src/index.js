4902d3d3174481dff677a421de585fa21caba450
Matteo Manchi <matteo.manchi@gmail.com>
2017-03-22 14:58:30 +0100

    Navigator with profile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React, { Component } from 'react';
-import {
-    AppRegistry,
-    Text,
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, Button, FlatList} from 'react-native';
+import { StackNavigator, } from 'react-navigation';
 
-class SampleApp extends Component {
+class MainScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome'
+    };
     render() {
+        const {navigate} = this.props.navigation;
         return (
-            <Text style={{margin: 40}}>
-                Hello, Codemotion!
-            </Text>
+            <Button
+                title="Go to Jane's profile"
+                onPress={() => navigate('Profile', {name: 'Jane'})}/>
         );
     }
 }
 
-AppRegistry.registerComponent('SampleApp', () => SampleApp);
+class ProfileScreen extends React.Component {
+    static navigationOptions = {
+        title: ({ state }) => `${state.params.name}'s profile`,
+        header: {
+           right: <Button title="Info" />,
+        },
+    };
+
+    render() {
+        const {goBack} = this.props.navigation;
+        return (
+            <Button
+                title="Back to main"
+                onPress={() => goBack()}/>
+        );
+    }
+}
+
+const App = StackNavigator({
+    Main: {screen: MainScreen},
+    Profile: {screen: ProfileScreen},
+});
+
+AppRegistry.registerComponent('SampleApp', () => App);
